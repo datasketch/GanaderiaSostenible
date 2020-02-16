48305f2fa47d30717445ece949a3db2c2e69f899
jpmarindiaz <jpmarindiaz@gmail.com>
2020-02-16 06:32:48 -0500

    escape unicode

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,16 @@
 ## code to prepare `DATASET` dataset goes here
+#usethis::use_data("DATASET")
 
-usethis::use_data("DATASET")
+
+path <- system.file("helpers", "co2_municipios.csv", package = "GanaderiaSostenible")
+data_mun <-  suppressMessages(readr::read_csv(path))
+
+#stringi::stri_escape_unicode(c("hola é", "ñ as"))
+library(tidyverse)
+
+data_mun <- data_mun %>%
+  mutate(NOMBRE_ENT = stringi::stri_escape_unicode(NOMBRE_ENT),
+         DEPARTAMEN = stringi::stri_escape_unicode(DEPARTAMEN))
+write_csv(data_mun, "inst/helpers/co2_municipios.csv")
+#stringi::stri_escape_unicode(c("hola é", "ñ as"))
+
